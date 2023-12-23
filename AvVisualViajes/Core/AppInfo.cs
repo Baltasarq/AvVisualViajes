d7f867d4362c2a4dece1690341a6c9c1b38c7b84
baltasar <baltasarq@gmail.com>
2023-12-23 22:49:25 +0100

    Bumps version

--- AvVisualViajes/Core/AppInfo.cs
+++ AvVisualViajes/Core/AppInfo.cs
@@ -5,6 +5,6 @@ namespace AvVisualViajes.Core {
     public static class AppInfo {
         public const string Name = "AvVisual Viajes";
         public const string Email = "jbgarcia@uvigo.es";
-        public const string Version = "1.0 20210925";
+        public const string Version = "1.1 20231223";
     }
 }
